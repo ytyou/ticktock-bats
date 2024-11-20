0ec2fe330f6c2c50035c87aca1755e4fd93a2de8
Yongtao You <yongtao.you@gmail.com>
2024-11-20 06:56:12 +0000

    add tests for special values NaN and Inf

--- tests/compress.sh
+++ tests/compress.sh
@@ -3,11 +3,14 @@
 . common.bash
 
 TS=`date +%s`
-TS1=$(( $TS - 100 ))
+TS1=$(( $TS - 1000 ))
 TS2=$(( $TS1 + 11 ))
 TS3=$(( $TS2 + 8 ))
 TS4=$(( $TS3 + 16 ))
 TS5=$(( $TS4 + 1 ))
+TS6=$(( $TS5 + 31 ))
+TS7=$(( $TS6 + 3 ))
+TS8=$(( $TS7 + 44 ))
 
 for i in {0..4}
 do
@@ -18,22 +21,28 @@ start_tt "--tsdb.compressor.version=$i"
 check_tt_running
 ping_tt
 
-api_put_tcp "put compression.metric $TS1 0.001 t1=v1"
+api_put_tcp "put compression.metric $TS1 nan t1=v1"
 check_status "$?"
-api_put_tcp "put compression.metric $TS2 1.234 t1=v1"
+api_put_tcp "put compression.metric $TS2 0.001 t1=v1"
 check_status "$?"
-api_put_tcp "put compression.metric $TS3 12345 t1=v1"
+api_put_tcp "put compression.metric $TS3 NaN t1=v1"
 check_status "$?"
-api_put_tcp "put compression.metric $TS4 12345 t1=v1"
+api_put_tcp "put compression.metric $TS4 1.234 t1=v1"
 check_status "$?"
-api_put_tcp "put compression.metric $TS5 99999 t1=v1"
+api_put_tcp "put compression.metric $TS5 12345 t1=v1"
+check_status "$?"
+api_put_tcp "put compression.metric $TS6 NaN t1=v1"
+check_status "$?"
+api_put_tcp "put compression.metric $TS7 99999 t1=v1"
+check_status "$?"
+api_put_tcp "put compression.metric $TS8 Inf t1=v1"
 check_status "$?"
 sleep 1
 
 RESP=`query_tt_get "start=1d-ago&m=none:compression.metric%7Bt1=v1%7D"`
 check_status "$?"
 echo "RESP=$RESP"
-check_output '[{"metric":"compression.metric","tags":{"t1":"v1"},"aggregateTags":[],"dps":{"'$TS1'":0.001,"'$TS2'":1.234,"'$TS3'":12345.0,"'$TS4'":12345.0,"'$TS5'":99999.0}}]' "$RESP"
+check_output '[{"metric":"compression.metric","tags":{"t1":"v1"},"aggregateTags":[],"dps":{"'$TS1'":nan,"'$TS2'":0.001,"'$TS3'":nan,"'$TS4'":1.234,"'$TS5'":12345.0,"'$TS6'":nan,"'$TS7'":99999.0,"'$TS8'":inf}}]' "$RESP"
 
 stop_tt
 wait_for_tt_to_stop
