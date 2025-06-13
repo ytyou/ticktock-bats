fd9654d432f48f4c908e6616082793382b6b1a9a
Yongtao You <yongtao.you@gmail.com>
2025-06-13 16:20:24 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+TGT=rc
+
+# process command line arguments
+while [[ $# -gt 0 ]]
+do
+    key=$1
+
+    case $key in
+        -b)
+        shift
+        TGT=$1
+        ;;
+
+        *)
+        break
+        ;;
+    esac
+    shift
+done
+
 if [ -z "$TT_SRC" ]; then
-    export TT_SRC=../ticktock
+    export TT_SRC=../$TGT
+fi
+
+PID=`pgrep -f -u $USER bin/tt`
+
+if [ ! -z "$PID" ]; then
+    echo "TickTockDB process ($PID) is running. Abort!"
+    exit 1
 fi
 
 echo "Testing against TickTockDB at $TT_SRC"
